e9aded9d12f1c8ae5d2eed88d1e97e5fac73f0b8
Example User <example@example.com>
2025-03-29 18:44:27 +0200

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // This will properly configure GitHub Pages paths
+  basePath: process.env.NODE_ENV === 'production' ? '/petru-tirla' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
